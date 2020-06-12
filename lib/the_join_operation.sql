e02059b720f5d76282e9dc80921af699dd838aa3
Buyaki01 <rittahbuyaki@gmail.com>
2020-06-12 15:50:57 +0300

    Add question 13 in the_join_operation.sql

--- lib/the_join_operation.sql
+++ lib/the_join_operation.sql
@@ -101,3 +101,12 @@ GROUP BY matchid, mdate
 -- QUESTION 13
 -- List every match with the goals scored by each team as shown. 
 -- This will use "CASE WHEN" which has not been explained in any previous exercises.
+-- Notice in the query given every goal is listed. 
+-- If it was a team1 goal then a 1 appears in score1, otherwise there is a 0. 
+-- You could SUM this column to get a count of the goals scored by team1. 
+-- Sort your result by mdate, matchid, team1 and team2.
+
+SELECT mdate, team1, SUM(CASE WHEN teamid = team1 THEN 1 ELSE 0 END) AS score1, team2, SUM(CASE WHEN teamid = team2 THEN 1 ELSE 0 END) AS score2 
+FROM game LEFT JOIN goal ON (id = matchid)
+GROUP BY mdate,team1,team2
+ORDER BY mdate, matchid, team1, team2
