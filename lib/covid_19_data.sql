f3b049b53735edcd5472c1b103132007b1cd9d11
Buyaki01 <rittahbuyaki@gmail.com>
2020-06-16 02:28:27 +0300

    Add question 3 and 4 in covid_19_data.sql

--- lib/covid_19_data.sql
+++ lib/covid_19_data.sql
@@ -26,3 +26,27 @@ ORDER BY whn
 -- The number of confirmed case is cumulative - but we can use LAG to recover the number of new cases reported for each day.
 -- Show the number of new cases for each day, for Italy, for March.
 
+SELECT name, DAY(whn),
+   confirmed - LAG(confirmed, 1) OVER (PARTITION BY name ORDER BY whn) AS new
+ FROM covid
+WHERE name = 'Italy'
+AND MONTH(whn) = 3
+ORDER BY whn
+
+-- QUESTION 4
+-- The data gathered are necessarily estimates and are inaccurate. 
+-- However by taking a longer time span we can mitigate some of the effects.
+-- You can filter the data to view only Monday's figures WHERE WEEKDAY(whn) = 0.
+-- Show the number of new cases in Italy for each week - show Monday only.
+
+SELECT name, DATE_FORMAT(whn,'%Y-%m-%d'), confirmed - LAG(confirmed, 1) OVER (PARTITION BY name ORDER BY whn) AS new_this_week
+ FROM covid
+WHERE name = 'Italy'
+AND WEEKDAY(whn) = 0
+ORDER BY whn
+
+-- QUESTION 5
+-- You can JOIN a table using DATE arithmetic. 
+-- This will give different results if data is missing.
+-- Show the number of new cases in Italy for each week - show Monday only.
+-- In the sample query we JOIN this week tw with last week lw using the DATE_ADD function.
